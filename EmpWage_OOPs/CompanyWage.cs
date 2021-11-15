b096f554913c2bc5a4dadccf1807cea0da1f0ff3
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-15 15:20:55 +0530

    Employee Wage of Multiple Companies using Interface Approach

---
+++ EmpWage_OOPs/CompanyWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Wage10
+{
+    public class CompanyWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+        public CompanyWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+
+        }
+        public string toString()
+        {
+            return "Total Employee Wage for company : " + this.company + " is : " + this.totalEmpWage;
+        }
+
+    }
+}
---
+++ EmpWage_OOPs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmpWage_OOPs
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 5, 10);
+            empWageBuilder.addCompanyEmpWage("Wipro", 25, 8, 20);
+            empWageBuilder.addCompanyEmpWage("TCS", 24, 7, 25);
+            empWageBuilder.ComputeEmpWage();
+            Console.ReadLine();
+        }
+	}
+}
