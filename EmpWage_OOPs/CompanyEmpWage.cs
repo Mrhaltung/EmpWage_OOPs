b260f015e107f31632a9d347334f3610c8f35340
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-15 15:49:33 +0530

    Total Wage when queried by Company

---
+++ EmpWage_OOPs/CompanyEmpWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmpWage_OOPs
+{
+    class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = empRatePerHour;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.totalEmpWage = 0;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string toString()
+        {
+            return "Total Empaloyee Wage for Company : " + this.company + " is : " + this.totalEmpWage;
+        }
+    }
+}
---
+++ EmpWage_OOPs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmpWage_OOPs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Wipro", 25, 5, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company : " + empWageBuilder.getTotalWage("DMart"));
+            Console.ReadLine();
+        }
+    }
+}
